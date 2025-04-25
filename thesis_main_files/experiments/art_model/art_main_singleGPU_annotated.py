ccec5a41144fc212bf2e0562b5af64edbe1a87ff
abhishekgupte94 <abhishekgupte@hotmail.com>
2025-04-25 01:18:40 +0100

    Added stuff

--- thesis_main_files/experiments/art_model/art_main_singleGPU_annotated.py
+++ thesis_main_files/experiments/art_model/art_main_singleGPU_annotated.py
@@ -18,7 +18,7 @@ def main():
     # -------------------- CONFIG --------------------
     config = {
         "csv_name": "training_data_two.csv",
-        "batch_size": 128,
+        "batch_size": 64,
         "learning_rate": 1e-4,
         "num_epochs": 1,
         "device" : torch.device("cuda" if torch.cuda.is_available() else "cpu")
