643fb8d66d77003ad40ebabc2cc5789262ead7c3
abhishekgupte94 <abhishekgupte@hotmail.com>
2025-05-08 03:01:50 +0100

    various changes

--- thesis_main_files/main_files/preprocessing/art_avdf/art/video_preprocessor_SAVE_FILES_MULTI_GPU.py
+++ thesis_main_files/main_files/preprocessing/art_avdf/art/video_preprocessor_SAVE_FILES_MULTI_GPU.py
@@ -67,11 +67,10 @@ class VideoPreprocessor_FANET:
         frame_size = (width, height)
 
         out_path = os.path.join(self.output_base_dir, f"{video_name}_lips_only.mp4")
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         out = cv2.VideoWriter(out_path, fourcc, fps, frame_size)
 
         if not out.isOpened():
-            print("[WARN] AVC1 codec not available. Trying avc1...")
             raise Exception("❌ All codecs failed: H264, avc1, and mp4v.")
         print(f"[INFO] Saving output to {out_path}")
 
